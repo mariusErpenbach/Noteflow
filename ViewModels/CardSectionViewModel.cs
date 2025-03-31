606d50298759d7da65970eeb9bbf35af576fc86a
Hawty1 <marius_erpenbach@web.de>
2025-03-31 10:12:25 +0200

    category filter

--- ViewModels/CardSectionViewModel.cs
+++ ViewModels/CardSectionViewModel.cs
@@ -1,7 +1,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Noteflow.Models;
 using Noteflow.Services;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Noteflow.ViewModels
 {
@@ -10,12 +12,37 @@ namespace Noteflow.ViewModels
         [ObservableProperty]
         private List<IndexCard> _cards;
 
+        [ObservableProperty]
+        private List<IndexCard> _allCards; // Originale, ungefilterte Liste
+
+        [ObservableProperty]
+        private string _searchText = string.Empty;
+
         private readonly CardBankManagement _cardBankManagement;
 
         public CardSectionViewModel(CardBankManagement cardBankManagement)
         {
             _cardBankManagement = cardBankManagement;
-            _cards = _cardBankManagement.LoadCards();
+            _allCards = _cardBankManagement.LoadCards();
+            _cards = new List<IndexCard>(_allCards); // Kopie der originalen Liste
+        }
+
+        partial void OnSearchTextChanged(string value)
+        {
+            if (value.Length < 3)
+            {
+                // Bei weniger als 3 Zeichen alle Karten anzeigen
+                Cards = new List<IndexCard>(AllCards);
+                return;
+            }
+
+            // Filter anwenden (case-insensitive)
+            var lowerValue = value.ToLower();
+            Cards = AllCards.Where(card =>
+                card.Front.ToLower().Contains(lowerValue) ||
+                card.Category.ToLower().Contains(lowerValue) ||
+                card.Back.ToLower().Contains(lowerValue)
+            ).ToList();
         }
     }
 }
--- obj/Debug/net9.0/Noteflow.AssemblyInfo.cs
+++ obj/Debug/net9.0/Noteflow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Noteflow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1abf6165a056fa733673a08eef8245f930c4d82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17991170aed4b09688c33b10dc4cf3113ab7aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Noteflow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Noteflow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
