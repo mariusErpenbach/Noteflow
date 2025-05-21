8ef42def22383228e409170be5fcb0c14504ef74
Hawty1 <marius_erpenbach@web.de>
2025-05-21 21:18:46 +0200

    new cards

--- obj/Debug/net9.0/Noteflow.AssemblyInfo.cs
+++ obj/Debug/net9.0/Noteflow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Noteflow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+936a355c0e38e8bb91571ae5a70e1172bc232315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec516f26456441a8d5dcd8fa6450921c786e0d70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Noteflow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Noteflow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
